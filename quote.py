10d7425a9d8d3edf377f7384c642d483e06dfeb7
mcdelux402 <mcdelux402@gmail.com>
2020-11-17 21:36:28 +0100

    [QUOTE] problème des champs vide résolu - retourne la liste d'objets quote

--- quote.py
+++ quote.py
@@ -24,6 +24,18 @@ class Quote:
         self.pl = pl
         self.ret = ret
         self.volume = volume
+    
+    def print_info(self):
+        print("close " + str(self.close))
+        print("coupon " + str(self.coupon))
+        print("gross " + str(self.gross))
+        print("high " + str(self.high))
+        print("low " + str(self.low))
+        print("nav " + str(self.nav))
+        print("open " + str(self.ope))
+        print("pl " + str(self.pl))
+        print("return " + str(self.ret))
+        print("close " + str(self.volume))
         
 
 def get_assets(endpointApi, date=None, full_response=False):
@@ -47,13 +59,24 @@ def asset_to_quotes(asset):
     obj_quotes = [] 
     result = get_quotes("/asset/", asset['ASSET_DATABASE_ID']['value'], "/quote")
     quotes = json.loads(result)
-    for quote in quotes:
-        print(quote)
-        new_quote = Quote(quote['close'], quote['coupon'], quote['date'], quote['gross'], quote['high'], quote['low'], quote['nav'], quote['open'], quote['pl'], quote['return'], quote['volume'])
-        obj_quotes.append(new_quote)
-    
-    return obj_quotes
 
+    for quote in quotes:
+        close = quote.get('close', 'none')
+        coupon = quote.get('coupon', 'none')
+        date = quote.get('date', 'none')
+        gross = quote.get('gross', 'none')
+        high = quote.get('high', 'none')
+        low = quote.get('low', 'none')
+        nav = quote.get('nav', 'none')
+        op = quote.get('open', 'none')
+        pl = quote.get('pl', 'none')
+        ret = quote.get('ret', 'none')
+        volume = quote.get('volume', 'none')
+
+        new_quote = Quote(close, coupon, date, gross, high, low, nav, op, pl, ret, volume)
+        obj_quotes.append(new_quote)
+
+    return obj_quotes
 
 #------------ GET ASSETS --------------
 actif = get_assets("/asset")
@@ -63,8 +86,13 @@ allActif = json.loads(actif)
 
 #------------ GET QUOTES -------------
 print("Mes quotes")
-allQuotes = asset_to_quotes(allActif[0])
-print(allQuotes)
+allQuotes = asset_to_quotes(allActif[1])
 
+allQuotes[0].print_info()
+
+#for elt in allQuotes:
+#    elt.print_info()
+   
+#print("len of my quote list " + str(len(allQuotes)))
             
     
