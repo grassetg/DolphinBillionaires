cc80494770639aa70e1082004696240e4d5f9caa
Guillaume Grasset <guillaume.grasset@epita.fr>
2020-11-18 11:20:01 +0100

    [models] remove factory and move function to container module

--- models/dynAmountLineContainer.py
+++ models/dynAmountLineContainer.py
@@ -40,3 +40,25 @@ def jsonToDynAmountLineContainer(jsonContainer):
 
         asset = DynAmountLineAsset.jsonToDynAmountLineAsset(dictionary['asset'])
         return DynAmountLineContainer(asset, None)
+
+
+def createCurrencyContainer(amount_: float, currency_: str = "EUR"):
+    """
+    :param amount_: amount invested
+    :param currency_: iso code of the currency
+    :return: the matching dynAmountLineContainer with currency
+    """
+
+    amountLineCurrency = DynAmountLineCurrency(amount_, currency_)
+    return DynAmountLineContainer(None, amountLineCurrency)
+
+
+def createAssetContainer(asset_: int, quantity_: float):
+    """
+    :param asset_: the asset's db id
+    :param quantity_: asset quantity
+    :return: the matching dynAmountLineContainer with asset
+    """
+
+    amountLineAsset = DynAmountLineAsset(asset_, quantity_)
+    return DynAmountLineContainer(amountLineAsset, None)
