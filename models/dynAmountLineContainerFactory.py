9c420776770828c5ee38b53c87cc2ae818df29ab
Guillaume Grasset <guillaume.grasset@epita.fr>
2020-11-18 11:17:53 +0100

    [models] update containerFactory

--- models/dynAmountLineContainerFactory.py
+++ models/dynAmountLineContainerFactory.py
@@ -8,15 +8,22 @@ class DynAmountLineContainerFactory:
     Factory for DynAmountLineContainers
     """
 
-    def create(self, asset_: int, quantity_: float, amount_: float, currency_: str = "EUR"):
+    def createAssetContainer(self, asset_: int, quantity_: float):
         """
         :param asset_: the asset's db id
         :param quantity_: asset quantity
+        :return: the matching dynAmountLineContainer with asset
+        """
+
+        amountLineAsset = DynAmountLineAsset(asset_, quantity_)
+        return DynAmountLineContainer(amountLineAsset, None)
+
+    def createCurrencyContainer(self, amount_: float, currency_: str = "EUR"):
+        """
         :param amount_: amount invested
         :param currency_: iso code of the currency
-        :return: the matching dynAmountLineContainer
+        :return: the matching dynAmountLineContainer with currency
         """
-        amountLineAsset = DynAmountLineAsset(asset_, quantity_)
-        amountLineCurrency = DynAmountLineCurrency(amount_, currency_)
 
-        return DynAmountLineContainer(amountLineAsset, amountLineCurrency)
+        amountLineCurrency = DynAmountLineCurrency(amount_, currency_)
+        return DynAmountLineContainer(None, amountLineCurrency)
