9af69feb1b7a7ed247f0e262f7c32befc9fdf627
Guillaume Grasset <guillaume.grasset@epita.fr>
2020-11-18 09:07:09 +0100

    Update models init.py

--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,5 @@ from models.dynAmountLineContainer import  DynAmountLineContainer
 from models.dynAmountLineContainerFactory import DynAmountLineContainerFactory
 from models.portfolio import Portfolio
 from models.ratio import Ratio
+from models.quote import *
+from models.asset import *
