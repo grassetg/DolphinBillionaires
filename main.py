8f6752ac155f4737943d381d07388532b1297768
MickaelHawat <mikahawat@gmail.com>
2020-11-24 16:52:43 +0100

    advance a bit

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from safePortfolio import *
 from copy import deepcopy
 from safePortfolio import enough_assets
     
-               
+print("--- START ---")
+
 assets = json.loads(get_assets(("ASSET_DATABASE_ID", "asset_fund_info_decimalisation")))
 assets_id = []
 assets_quantity = {}
@@ -22,80 +23,81 @@ for i in range(nb_asset):
 
 
 
-def init_ptfs(nb : int = 50):
-    ptfs = []
-    for i in range(nb):
-        list_id = []
-        all_asset_id_s = all_asset_id
-        for j in range(randint(15, 30)):
-            choice = randint(0, nb_asset - 1 - j)
-            list_id.append(all_asset_id_s[choice])
-            all_asset_id_s.pop(choice)
-        ptfs.append(list_id)        
-    return ptfs
-
 
 def ptfs_mutation(ptf, nb : int = 50):
     ptfs = []
-    ptfs.append(ptf) #ajouter le ptf original/model
-    #création d'une liste avec la liste des id des actifs du portefeuilles
+    ptfs.append(ptf)
     len_ptf = len(ptf)
-    #faire pop tous les id de ptf_asset de la liste d'asset à ajouter pour les mutations
-    all_asset_id_not_in_ptf = all_asset_id
+    asset_id_not_in_ptf = deepcopy(assets_id)
     for i in range(len_ptf):
-        all_asset_id_not_in_ptf.pop(all_asset_id_not_in_ptf.index(ptf[i]))
+        asset_id_not_in_ptf.pop(asset_id_not_in_ptf.index(ptf[i]))
     for i in range(nb):
-        list_id = ptf
-        all_asset_id_s = all_asset_id_not_in_ptf
+        list_id = deepcopy(ptf)
+        asset_id_s = deepcopy(asset_id_not_in_ptf)
         for j in range(randint(1, 5)): #choisir le nombre d'asset qui vont changer
             change_in = randint(0, len_ptf)
             if change_in == len_ptf:
                 choice = randint(0, nb_asset - 1 - j)
-                list_id.append(all_asset_id_s[choice])
-                all_asset_id_s.pop(choice)    
+                list_id.append(asset_id_s[choice])
+                asset_id_s.pop(choice)    
             else :
                 choice = randint(0, nb_asset - 1 - j)
-                list_id[change_in] = all_asset_id_s[choice]
-                all_asset_id_s.pop(choice)
+                list_id[change_in] = asset_id_s[choice]
+                asset_id_s.pop(choice)
         ptfs.append(list_id)                
     return ptfs
 
 
-'''
+
 def init_ptfs(nb: int = 50):
     ptfs = []
+    ptfs_id = []
     for i in range(nb):
         list_id = []
         asset_id_copy = deepcopy(assets_id)
         values = []
-
         for j in range(randint(15, 30)):
             choice = randint(0, nb_asset - 1 - j)
             asset_id = asset_id_copy[choice]
             list_id.append(asset_id)
             asset_id_copy.pop(choice)
-
             if asset_id in assets_quantity:
                 quantity = assets_quantity[asset_id]
             else:
                 quantity = 1
-
             container = createAssetContainer(asset_id, quantity)
             values.append(container)
+        ptfs_id.append(list_id)
         ptfs.append(Portfolio(PORTFOLIO_NAME, values, "EUR", "front"))
+    return ptfs, ptfs_id
 
-    return ptfs
+
+(porfolios, porfolios_id) = init_ptfs(3)
+
+
+'''
+print(porfolios)
+print()
+print(porfolios_id[0])
+print()
+mut_porfolio = ptfs_mutation(porfolios_id[0],1)
+print(mut_porfolio[1])
 '''
 
-porfolios = init_ptfs(10)
+#Rle problème st qu'on a 0 asset dans notre ptf
+port = json.dumps({"label":"EPITA_PTF_3","currency":{"code":"EUR"},"type":"front","values":{"2016-06-01":[{"asset":{"asset":1845,"quantity":1.0}}]}})
+print(put_portfolio(1822, port))
+print(get_portfolio(str(1822)))
 
-'''
+
 def get_best_candidate(portfolios):
     notes = []
     for portfolio in portfolios:
-        print(portfolio.toJson())
-        raise Exception("stop")
-        put_portfolio(PORTFOLIO_ID, portfolio.toJson())
+        print("I AM HERE")
+        json_portfolio = portfolio.toJson()
+        print(json_portfolio)
+        #raise Exception("stop")
+        put_portfolio(PORTFOLIO_ID, json_portfolio)
         print("PUT PORTFOLIO")
         sharpe = json.loads(get_sharpe(PORTFOLIO_ID, start_date="2016-06-01", end_date_="2020-09-30"))
         notes.append(sharpe["1822"]["12"]["value"])
@@ -105,11 +107,13 @@ def get_best_candidate(portfolios):
     print("all : " + str(notes))
     print()
     print("max : " + str(notes[index]))
-    return portfolios[index]
+    return index
 
 
-print(get_best_candidate(porfolios))
-'''
+print(porfolios[get_best_candidate(porfolios)])
+
+#port = json.dumps({"label":"EPITA_PTF_3","currency":{"code":"EUR"},"type":"front","values":{"2016-06-01":[{"asset":{"asset":1845,"quantity":1.0}}]}})
+#put_portfolio(1822, compo)
 
 # creation du portefeuille
 # ptf = Portfolio(vars.PORTFOLIO_NAME, values_, currency_ , type_)
